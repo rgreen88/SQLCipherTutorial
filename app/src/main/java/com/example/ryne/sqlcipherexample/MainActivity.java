33ae63f9f71891af4cf795b1bc367f4c04653e31
rgreen88 <rynelgreen@gmail.com>
2018-01-09 13:57:41 -0500

    first commit, added sqlcipher dependency

---
+++ app/src/main/java/com/example/ryne/sqlcipherexample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ryne.sqlcipherexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
